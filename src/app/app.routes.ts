964d3a5519e5c374922e180a7d8ea8bd1bbcad0b
Adam Combs <adam.combs.osv@fedex.com>
2024-06-10 20:43:55 -0400

    no path redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AllCardsComponent } from './all-cards/all-cards.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/all-cards', pathMatch: 'full'},
     { path: 'all-cards', component: AllCardsComponent },
 ];
